630de7f2e95f0bb72a562b2fc17f14b4a3856ae6
muramurada <shikitaro1192@gmail.com>
2024-12-19 10:08:09 +0900

    html and css have beeen validated

--- js/common.js
+++ js/common.js
@@ -5,11 +5,6 @@
 		$('.section-title-question').click(function(){
 			$(this).toggleClass('active');
 			$(this).next('.section-title-answer').slideToggle();
-    //   if($(this).hasClass('active')){
-		// 	$(this).next().fadeTo(1000, 0);
-    // } else {
-		// 	$(this).next().fadeTo(1000, 1);
-    // }
 		});
 		//ホバーで動く
 		// $('.section-title-about').hover(function(){
